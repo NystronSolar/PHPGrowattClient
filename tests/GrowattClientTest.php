8a3747f02a89cd9aaa062f380e69c0e2013e3407
TheDevick <erick@bilhalba.com.br>
2022-12-13 03:03:49 -0300

    test: create Growatt Client Test
    Create a Growatt Client Test with an function that creates an Growatt Client object with the Test
    Values (ApiKey and ApiURL)

---
+++ tests/GrowattClientTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use NystronSolar\PHPGrowattClient\GrowattClient;
+use PHPUnit\Framework\TestCase;
+
+class GrowattClientTest extends TestCase
+{
+    private string $apiToken = "6eb6f069523055a339d71e5b1f6c88cc"; // API Token used for tests
+
+    private string $apiUrl = "https://test.growatt.com/v1/"; // API URL used for tests
+
+    /**
+     * Create a Growatt Client object
+     * @return GrowattClient
+     */
+    private function createGrowattClient(): GrowattClient
+    {
+
+        $growattClient = new GrowattClient($this->apiToken, $this->apiUrl);
+
+        return $growattClient;
+    }
+}
